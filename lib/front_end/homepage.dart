f5550212f89cb58d5e1d0b88569331e85d85edba
terneko <jirathonchaisuk@kkumail.com>
2022-01-24 10:41:45 +0700

    :chart_with_upwards_trend: Update Homepage

--- lib/front_end/homepage.dart
+++ lib/front_end/homepage.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Container(
+        body: SingleChildScrollView(
           padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
           child: Column(children: [
             /*  Row(
@@ -223,45 +223,279 @@ class _HomePageState extends State<HomePage> {
              * 
              * 
             */
-
             Container(
-              margin: EdgeInsets.fromLTRB(5, 0, 5, 5),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: color.AppColor.WidgetBackground.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20), //border corner radius
-              ),
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Row(
+                margin: EdgeInsets.fromLTRB(5, 0, 5, 5),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: color.AppColor.WidgetBackground.withOpacity(0.5),
+                  borderRadius:
+                      BorderRadius.circular(20), //border corner radius
+                ),
+                alignment: Alignment.center,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(20, 10, 0, 5),
-                        child: Text('Finished'),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "Finished",
+                            style: TextStyle(
+                              color: color.AppColor.NameWidget,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(1.0, 3.0),
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              height: 50,
+                              width: double.maxFinite,
+                              decoration: BoxDecoration(
+                                  color: color.AppColor.box_class,
+                                  borderRadius: BorderRadius.circular(15),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        offset: Offset(0, 5),
+                                        blurRadius: 5,
+                                        color: Colors.grey.withOpacity(0.8))
+                                  ]),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            "Circuit Signal System",
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          )
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 15,
+                                          ),
+                                          Text(
+                                            "9:00 - 10:30 AM",
+                                            style: TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.normal),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Expanded(child: Container()),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                    width: 50,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                        color: color.AppColor.offline,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              offset: Offset(0, 5),
+                                              blurRadius: 5,
+                                              color: Colors.grey.withOpacity(1))
+                                        ]),
+                                    child: Center(
+                                      child: Text(
+                                        "Join",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            )
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                  Container(
-                      height: 100,
-                      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: color.AppColor.box_class,
-                        borderRadius: BorderRadius.circular(15),
-                      ))
-                ],
-              ),
-            ),
+                )),
             Container(
-              margin: EdgeInsets.fromLTRB(5, 0, 5, 5),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: color.AppColor.WidgetBackground.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20), //border corner radius
-              ),
-              alignment: Alignment.center,
-            )
+                margin: EdgeInsets.fromLTRB(5, 0, 5, 5),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: color.AppColor.WidgetBackground.withOpacity(0.5),
+                  borderRadius:
+                      BorderRadius.circular(20), //border corner radius
+                ),
+                alignment: Alignment.center,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "During",
+                            style: TextStyle(
+                              color: color.AppColor.NameWidget,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(1.0, 3.0),
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              height: 50,
+                              width: double.maxFinite,
+                              decoration: BoxDecoration(
+                                  color: color.AppColor.box_class,
+                                  borderRadius: BorderRadius.circular(15),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        offset: Offset(0, 5),
+                                        blurRadius: 5,
+                                        color: Colors.grey.withOpacity(0.8))
+                                  ]),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            "Digital Logic Design",
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          )
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 15,
+                                          ),
+                                          Text(
+                                            "9:00 - 10:30 AM",
+                                            style: TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.normal),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Expanded(child: Container()),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                    width: 50,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              color.AppColor.Gradient3,
+                                              color.AppColor.Gradient3
+                                                  .withOpacity(0.8),
+                                              color.AppColor.Gradient4
+                                                  .withOpacity(0.8),
+                                              color.AppColor.Gradient4,
+                                              //add more colors for gradient
+                                            ],
+                                            begin: Alignment
+                                                .topRight, //begin of the gradient color
+                                            end: Alignment
+                                                .bottomLeft, //end of the gradient color
+                                            stops: [
+                                              0,
+                                              0.1,
+                                              0.9,
+                                              1
+                                            ] //stops for individual color
+                                            //set the stops number equal to numbers of color
+                                            ),
+                                        color: color.AppColor.offline,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              offset: Offset(0, 5),
+                                              blurRadius: 5,
+                                              color:
+                                                  Colors.grey.withOpacity(1)),
+                                        ]),
+                                    child: Center(
+                                      child: Text(
+                                        "Join",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )),
           ]),
         ),
       );
