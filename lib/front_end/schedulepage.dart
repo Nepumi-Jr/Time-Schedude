391727d62c23b7e7ee8bfe289d48fe2a99acfbc3
terneko <jirathonchaisuk@kkumail.com>
2022-01-24 11:12:01 +0700

    :hammer: Do That

---
+++ lib/front_end/schedulepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class schedule extends StatefulWidget {
+  const schedule({Key? key}) : super(key: key);
+
+  @override
+  _scheduleState createState() => _scheduleState();
+}
+
+class _scheduleState extends State<schedule> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+          color: Colors.red,
+          child: Row(
+            children: [],
+          )
+
+          /*
+          * * write your code here
+          **/
+          ),
+    );
+  }
+}
