d3a972cf6b53126300d012eeeed4d3d56d0bdad0
Nantawat <nantawat.kho@kkumail.com>
2022-02-09 13:49:05 +0700

    subject

--- lib/back_end/subject.dart
+++ lib/back_end/subject.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+//import 'package:path_provider/path_provider.dart';
 
 class Subject {
   late String name;
@@ -34,23 +36,104 @@ class Subject {
         'timeStart': timeStart,
         'timeEnd': timeEnd
       };
+
+  String get getname => name;
+  set setname(String name) => this.name = name;
 }
 
-List<String> allSubject = [];
+List<String> timetable = [];
+List<List<double>> timeSubject = [];
 late String subjectDelete;
+late String nameEditSubject;
+late String nameSubjectDelete;
+late int numSubject;
+
+/*Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/subject.json');
+}
+
+Future<File> writeSubject(String subject) async {
+  final file = await _localFile;
+
+  // Write the file
+  return file.writeAsString(subject);
+}
+
+Future<String> readSubject() async {
+  try {
+    final file = await _localFile;
+
+    // Read the file
+    final contents = await file.readAsString();
+
+    return contents;
+  } catch (e) {
+    // If encountering an error
+    return 'encountering an error';
+  }
+}*/
+
+void saveSubject() {}
+
+void loadSubject() {}
+
+void insertSubject() {}
 
 void addSubject(Subject subject) {
-  allSubject.add(jsonEncode(subject.toJson()));
+  timetable.add(jsonEncode(subject.toJson()));
+  timeSubject.add([subject.timeStart, subject.timeEnd]);
 }
 
 void deleteSubject(Subject subject) {
-  subjectDelete = jsonEncode(subject.toJson());
-  for (int i = 0; i < allSubject.length; i++) {
-    if (subjectDelete == allSubject[i]) {
-      print(i);
-      allSubject.removeAt(i);
+  nameSubjectDelete = subject.getname;
+  List<double> timeLearn = [];
+  timeLearn.add(subject.timeStart);
+  timeLearn.add(subject.timeEnd);
+
+  for (int i = 0; i < timetable.length; i++) {
+    var nameSubject = Subject.fromJson(jsonDecode(timetable[i]));
+    if (nameSubjectDelete == nameSubject.name) {
+      if (timeLearn.join(",") == timeSubject[i].join(",")) {
+        numSubject = i;
+      }
     }
   }
+
+  timetable.removeAt(numSubject);
+
+  /* subjectDelete = jsonEncode(subject.toJson());
+  for (int i = 0; i < timetable.length; i++) {
+    if (subjectDelete == timetable[i]) {
+      timetable.removeAt(i);
+    }
+  }*/
+}
+
+void editSubject(Subject subject) {
+  // ลบ แล้วเพิ่มใหม่?
+  subjectDelete = jsonEncode(subject.toJson());
+  subject.name = 'ggggg';
+
+  /*nameEditSubject = subject.getname;
+  List<double> timeLearn = [];
+  timeLearn.add(subject.timeStart);
+  timeLearn.add(subject.timeEnd);
+
+  for (int i = 0; i < timetable.length; i++) {
+    var nameSubject = Subject.fromJson(jsonDecode(timetable[i]));
+    if (nameEditSubject == nameSubject.name) {
+      if (timeLearn.join(",") == timeSubject[i].join(",")) {
+        numSubject = i;
+      }
+    }
+  }*/
 }
 
 void main(List<String> args) {
@@ -64,20 +147,33 @@ void main(List<String> args) {
       'mathhhh', 'www.youtube.com', 'online', 'monday', 9.00, 10.30);
   var subjec5 = Subject.addSubject(
       'math', 'www.youtube.com', 'online', 'monday', 9.00, 10.30);
+  var subjec6 = Subject.addSubject(
+      'mathhhhhh', 'www.youtube.com', 'online', 'monday', 13.00, 14.00);
 
   addSubject(subjec1);
+  addSubject(subjec6);
+  //addSubject(subjec2);
+  //addSubject(subjec3);
+  //addSubject(subjec4);
+
+  //editSubject(subjec1);
 
   //allSubject.add(jsonEncode(subjec1.toJson()));
-  allSubject.add(jsonEncode(subjec2.toJson()));
-  allSubject.add(jsonEncode(subjec3.toJson()));
-  allSubject.add(jsonEncode(subjec4.toJson()));
-  allSubject.add(jsonEncode(subjec5.toJson()));
+  //timetable.add(jsonEncode(subjec2.toJson()));
+  //timetable.add(jsonEncode(subjec3.toJson()));
+  //timetable.add(jsonEncode(subjec4.toJson()));
+  //timetable.add(jsonEncode(subjec5.toJson()));
 
-  var deleteSubject1 = Subject.deleteSubject(
-      'math', 'www.youtube.com', 'online', 'monday', 9.00, 10.30);
+  /*var deleteSubject1 = Subject.deleteSubject(
+      'math', 'www.youtube.com', 'online', 'monday', 9.00, 10.30);*/
+  var deleteSubject2 = Subject.deleteSubject(
+      'mathhhhhh', 'www.youtube.com', 'online', 'monday', 13.00, 14.00);
   //subjectDelete = jsonEncode(deleteSubject1.toJson());
 
-  deleteSubject(deleteSubject1);
+  //deleteSubject(deleteSubject1);
+  deleteSubject(deleteSubject2);
+
+  editSubject(subjec1);
 
   /*for (int i = 0; i < allSubject.length; i++) {
     if (subjectDelete == allSubject[i]) {
@@ -86,11 +182,11 @@ void main(List<String> args) {
     }
   }*/
 
-  print(allSubject);
+  print(timetable);
 
-  print(allSubject[0]);
-  var user = Subject.fromJson(jsonDecode(allSubject[0]));
+  /*print(timetable[0]);
+  var user = Subject.fromJson(jsonDecode(timetable[0]));
   print(user.name);
   print(user.link);
-  print(user.learnAt);
+  print(user.learnAt);*/
 }
