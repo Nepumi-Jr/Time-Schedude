5520df7a8b1344e969942c8e1a7e47bfae9070d2
Nepumi-Jr <peerator@kkumail.com>
2022-03-20 21:28:12 +0700

    :recycle: Enum and ✨getLinkFromName

--- lib/back_end/link_check.dart
+++ lib/back_end/link_check.dart
@@ -5,6 +5,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+//? Code from so's
+List<String> getLinks(String input) {
+  List<String> myList = [];
+  final matcher = new RegExp(
+      r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+      caseSensitive: false,
+      multiLine: false);
+  int i = 0;
+  while ((matcher.hasMatch(input))) {
+    String url = matcher.stringMatch(input).toString();
+    myList.add(url);
+    i++;
+    input = input.replaceAll(url, "");
+  }
+
+  return myList;
+}
 
 Link_Check(String input) {
   int i = 0;
@@ -16,30 +33,34 @@ Link_Check(String input) {
   final matcher = new RegExp(
       r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
       caseSensitive: false,
-      multiLine: false);                                          //check link in text
-      myList[i] = matcher.stringMatch(input).toString();          //first link
-  while ((matcher.hasMatch(input)) == true) {                     //if there have link in text
-    app = Linkify(                                                //check for link can launch
-    onOpen: (link) async {
-      if (await canLaunch(link.url)) {
-        await launch(link.url);
-      } else {
-        throw 'Could not launch $link';
-      }
-    },
-    text: myList[i],
-    style: TextStyle(color: Colors.black),
-    linkStyle: TextStyle(color: Colors.blue),
-  );
+      multiLine: false); //check link in text
+  myList[i] = matcher.stringMatch(input).toString(); //first link
+  while ((matcher.hasMatch(input)) == true) {
+    //if there have link in text
+    app = Linkify(
+      //check for link can launch
+      onOpen: (link) async {
+        if (await canLaunch(link.url)) {
+          await launch(link.url);
+        } else {
+          throw 'Could not launch $link';
+        }
+      },
+      text: myList[i],
+      style: TextStyle(color: Colors.black),
+      linkStyle: TextStyle(color: Colors.blue),
+    );
     i = i + 1;
-    input = input.replaceAll(matcher.stringMatch(input).toString(), "");      //เป็น text ที่ตัด link อันแรกแล้ว
-    myList[i] = matcher.stringMatch(input).toString();                        //link จะถูกเก็บในarray mylist
-    
+    input = input.replaceAll(matcher.stringMatch(input).toString(),
+        ""); //เป็น text ที่ตัด link อันแรกแล้ว
+    myList[i] =
+        matcher.stringMatch(input).toString(); //link จะถูกเก็บในarray mylist
+
   }
-  while (j != i){
+  while (j != i) {
     print(j);
     all_Linkchecked = all_Linkchecked + "\n" + myList[j];
-    j=j+1;
+    j = j + 1;
   }
   app = Linkify(
     onOpen: (link) async {
